feaf7d1b380ff3d1deec8c41771f4ecb20bf6c85
Geraldo Nascimento <gnascim@gmail.com>
2024-08-12 21:18:45 +0100

    delete unrelated files

--- src/VHS/Video.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by Geraldo Nascimento on 03/08/2024.
-//
-
-#include "Video.h"
-    Video::Video(int id, std::string title, std::string genre) : videoId(id), videoTitle(title), videoGenre(genre) {}
-
-    int Video::getVideoId() const { return videoId; }
