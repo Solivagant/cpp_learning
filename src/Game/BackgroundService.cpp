8c9466c507daf0b6b8ada53baf4425a75a68698e
Geraldo Nascimento <gnascim@gmail.com>
2024-08-21 13:14:59 +0100

    reset generated backgrounds on death

--- src/Game/BackgroundService.cpp
+++ src/Game/BackgroundService.cpp
@@ -4,15 +4,17 @@
 
 #include "BackgroundService.h"
 
-void BackgroundService::Init(int screenWidth, int screenHeight, PlayerData* playerData) {
+void BackgroundService::Init(int screenWidth, int screenHeight, PlayerData* playerData, GameData* gameData) {
     this->backgroundWidth = screenWidth + 20;
     this->backgroundHeight = screenHeight + 20;
     this->playerData = playerData;
-    Image image = GenImageGradientLinear(backgroundWidth, this->backgroundHeight, 0, PURPLE, BLUE);
-    background1 = LoadTextureFromImage(image);
+    this->gameData = gameData;
+    Image image1 = GenImageGradientLinear(backgroundWidth, this->backgroundHeight, 0, PURPLE, BLUE);
+    background1 = LoadTextureFromImage(image1);
 
     Image image2 = GenImageGradientLinear(backgroundWidth, this->backgroundHeight, 0, PURPLE, BLUE);
-    background3 = LoadTextureFromImage(image2);
+    background2 = LoadTextureFromImage(image2);
+    UnloadImage(image1);
     UnloadImage(image2);
 }
 
@@ -25,7 +27,7 @@ void BackgroundService::Draw(float deltaTime) {
         nextBGAlpha += 0.05f * transitionTime;
 
         if (prevBGAlpha <= 0) {
-            background1 = background3;
+            background1 = background2;
             transitionStarted = false;
 
             prevBGAlpha = 1;
@@ -33,11 +35,16 @@ void BackgroundService::Draw(float deltaTime) {
         }
     }
 
-    DrawTexture(background1, playerPos.x - backgroundWidth / 2.0f, playerPos.y - this->backgroundHeight / 2.0f,
-                Fade(LIGHTGRAY, prevBGAlpha));
+    float posX = -10;
+    float posY = -10;
 
-    DrawTexture(background3, playerPos.x - backgroundWidth / 2.0f, playerPos.y - this->backgroundHeight / 2.0f,
-                Fade(LIGHTGRAY, nextBGAlpha));
+    if (gameData->GetCameraFollowsPlayer()){
+        posX = playerPos.x - this->backgroundWidth / 2.0f;
+        posY = playerPos.y - this->backgroundHeight / 2.0f;
+    }
+
+    DrawTexture(background1, posX, posY, Fade(LIGHTGRAY, prevBGAlpha));
+    DrawTexture(background2,  posX, posY, Fade(LIGHTGRAY, nextBGAlpha));
 }
 
 void BackgroundService::StartTransition() {
@@ -68,13 +75,31 @@ void BackgroundService::StartTransition() {
             break;
     }
 
-    background3 = LoadTextureFromImage(image);
+    background2 = LoadTextureFromImage(image);
     UnloadImage(image);
+}
+
+void BackgroundService::Restart() {
+    UnloadTexture(background1);
+    UnloadTexture(background2);
+
+    Image image1 = GenImageGradientLinear(backgroundWidth, this->backgroundHeight, 0, PURPLE, BLUE);
+    background1 = LoadTextureFromImage(image1);
+
+    Image image2 = GenImageGradientLinear(backgroundWidth, this->backgroundHeight, 0, PURPLE, BLUE);
+    background2 = LoadTextureFromImage(image2);
+    UnloadImage(image1);
+    UnloadImage(image2);
+
+    transitionStarted = false;
+
+    prevBGAlpha = 1;
+    nextBGAlpha = 0;
 }
 
 void BackgroundService::Unload() {
     UnloadTexture(background1);
-    UnloadTexture(background3);
+    UnloadTexture(background2);
 }
 
 
--- src/Game/BackgroundService.h
+++ src/Game/BackgroundService.h
@@ -7,22 +7,24 @@
 #include "../../lib/raylib.h"
 #include "../Util/MathUtil.h"
 #include "../Stats/PlayerData.h"
+#include "GameData.h"
 #include <map>
 
 class BackgroundService {
 public:
-    void Init(int screenWidth, int screenHeight, PlayerData* playerData);
+    void Init(int screenWidth, int screenHeight, PlayerData* playerData, GameData* gameData);
     void Draw(float deltaTime);
     void Unload();
     void StartTransition();
+    void Restart();
 private:
     Texture2D background1;
-
-    Texture2D background3;
+    Texture2D background2;
 
     int backgroundWidth;
     int backgroundHeight;
     PlayerData* playerData;
+    GameData* gameData;
 
     bool transitionStarted;
     float prevBGAlpha = 1;
@@ -32,7 +34,6 @@ private:
     std::vector<Color> colorsA{YELLOW,
                               GOLD,
                               ORANGE,
-                              PINK,
                               LIME,
                               DARKGREEN
     };
